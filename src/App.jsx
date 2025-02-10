d4ac931902a5672b536a8c75c0a2290bfe66cd09
DeepshikhaJana <deepshikhajana2004@gmail.com>
2025-02-11 02:32:29 +0530

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-
 import "./App.css";
-
 import Navbar from "./components/navbar";
+import {Routes, Route} from "react-router-dom"
+import Details from "./pages/details";
+import Favorites from "./pages/favorites";
+import Home from "./pages/home";
 
 
 function App() {
@@ -9,6 +11,11 @@ function App() {
     <div>
       <div className="min-h-screen p-6 bg-black text-gray-600 text-lg">
         <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+          <Route path="/recipe-item/:id" element={<Details />} />
+        </Routes>
       </div>
     </div>
   );
