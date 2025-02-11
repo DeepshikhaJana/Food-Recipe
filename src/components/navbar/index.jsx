d3ca0cf609c987cfa694c70a539393581679e8e2
DeepshikhaJana <deepshikhajana2004@gmail.com>
2025-02-12 00:59:25 +0530

    updated context

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
+import { useContext } from 'react';
 import { NavLink } from "react-router-dom";
+import { GlobalContext } from '../../context';
 
 function navbar() {
+  const {searchParam, setSearchParam, handleSubmit} = useContext(GlobalContext);
+  console.log(searchParam);
   return (
     <nav className='sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80"'>
       <div className="flex justify-between items-center">
         <div className="text-3xl font-bold text-white">Food Recipe 🍜</div>
-        <form>
+        <form onSubmit={handleSubmit}>
           <input
             type="text"
             name="search"
+            value={searchParam}
+            onChange={(event)=> setSearchParam(event.target.value)}
             placeholder="Enter Items..."
             className="bg-white/75 p-3 px-8 rounded-full outline-none lg:w-96 shadow-lg shadow-gray-600 focus:shadow-red-100"
           />
@@ -37,4 +43,4 @@ function navbar() {
   );
 }
 
-export default navbar
+export default navbar;
