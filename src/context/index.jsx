80334c2e6e8823f151915c4b82767dc5524bdbce
DeepshikhaJana <deepshikhajana2004@gmail.com>
2025-02-13 00:53:23 +0530

    added recipe details

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -7,7 +7,8 @@ export const GlobalContext = createContext(null);
 const GlobalState = ({children}) => {
     const [searchParam, setSearchParam] = useState("");
     const [loading, setLoading] = useState(false);
-    const [recipeList, setRecipeList] = useState([])
+    const [recipeList, setRecipeList] = useState([]);
+    const [recipeDetailsData, setRecipeDetailsData] = useState(null);
 
     async function handleSubmit(event){
         event.preventDefault()
@@ -31,9 +32,19 @@ const GlobalState = ({children}) => {
 
     console.log(loading, recipeList);
     return (
-        <GlobalContext.Provider value={{searchParam, loading, recipeList, setSearchParam, handleSubmit}}>
-            {children}
-        </GlobalContext.Provider>
+      <GlobalContext.Provider
+        value={{
+          searchParam,
+          loading,
+          recipeList,
+          setSearchParam,
+          handleSubmit,
+          recipeDetailsData,
+          setRecipeDetailsData,
+        }}
+      >
+        {children}
+      </GlobalContext.Provider>
     );
 }
 
